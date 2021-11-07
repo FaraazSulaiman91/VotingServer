b27e33d16f1b58d25e4e72f09f956460d3c966e7
Faraaz Sulaiman <faraazs@soliditech.com>
2021-11-07 22:40:50 +0200

    Clean up question API urls

--- src/main/java/com/faraazs/votingserver/question/QuestionController.java
+++ src/main/java/com/faraazs/votingserver/question/QuestionController.java
@@ -1,7 +1,6 @@
 package com.faraazs.votingserver.question;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -12,25 +11,26 @@ import java.util.List;
 @RestController
 public class QuestionController
 {
+	public static final String BASE_PATH = "/v1/questions";
 	private QuestionService questionService;
 
 	QuestionController(QuestionService questionService) {
 		this.questionService = questionService;
 	}
 
-	@GetMapping("/questions")
+	@GetMapping(BASE_PATH)
 	public List<Question> getQuestions(@RequestHeader long sessionCode)
 	{
 		return questionService.list(sessionCode);
 	}
 
-	@GetMapping("/questions/current")
+	@GetMapping(BASE_PATH + "/current")
 	public Question getCurrentQuestion(@RequestHeader long sessionCode)
 	{
 		return questionService.getLatest(sessionCode);
 	}
 
-	@PostMapping("/questions")
+	@PostMapping(BASE_PATH)
 	public Question createQuestion(@RequestHeader long sessionCode, @RequestBody Question question)
 	{
 		return questionService.create(sessionCode, question);
